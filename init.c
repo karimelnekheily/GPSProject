a486af1091ed1e7e0d9643d2b963484bc72f0c75
karimelnekheily <110358906+karimelnekheily@users.noreply.github.com>
2023-04-24 09:48:43 +0200

    Update and rename UART_init.c to init.c

--- init.c
+++ init.c
@@ -100,4 +100,29 @@ void setCLkFreq(uint8_t f){
     case 80:
         SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
     }
+/**
+ * @brief Initializes the GPIO Port F.
+ *
+ * Enables the clock for Port F and waits for it to be ready. Unlocks Port F and disables the analog function for pins PF0 to PF4.
+ * Allows changes to these pins and sets them to have no alternative function. The direction of PF1, PF2, and PF3 is set to output
+ * while the others are set to input. The pins are set to digital and pull-up resistors are enabled for PF0 and PF4. Finally, the LEDs are turned off.
+ *
+ * @param void
+ * @return void
+ */
+void GPIO_PF_init(void){
+  SYSCTL_RCGCGPIO_R |= 0x20;           // enable Clock for portF
+  while( (SYSCTL_RCGCGPIO_R&0x20)==0){}// Delay
+  GPIO_PORTF_LOCK_R= 0x4C4F434B;       // Unlock portF
+  GPIO_PORTF_AMSEL_R &= ~0x1F;         // disable analog function for pf0,pf1,pf2,pf3,pf4
+  GPIO_PORTF_CR_R|= 0x1F;              // allow changes for pf0,pf1,pf2,pf3,pf4
+  GPIO_PORTF_PCTL_R &= ~0x000FFFFF;    // configure  pf0,pf1,pf2,pf3,pf4 as GPIO
+  GPIO_PORTF_AFSEL_R &= ~0x1F;         // no alternative function
+  GPIO_PORTF_DIR_R = 0x0E;             // pf1,pf2,pf3: outputs, else inputs
+  GPIO_PORTF_DEN_R = 0x1F;             // pf0,pf1,pf2,pf3,pf4: digital
+  GPIO_PORTF_PUR_R =0x11;              // pull up resistor enabled for pf0,pf4
+  GPIO_PORTF_DATA_R &= 0x0E;           // LEDs are off
+}
+
+
 }
